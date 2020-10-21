6e4a23c65e0a3ad8e8207fb459e4b715a800bda9
FranciscoMaik <chiconunes3081@gmail.com>
2020-10-21 09:43:25 -0300

    Adiçao do banco de dados e implementaçao da rota post/transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,18 +1,30 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const listTransaction = new TransactionsRepository();
+  // const { transactions, balance } = await listTransaction.
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransation = new CreateTransactionService();
+
+  const transaction = await createTransation.execute({
+    title,
+    type,
+    value,
+    category
+  })
+
+  return response.json(transaction)
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm'
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: "outcome" | "income";
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
+    const transitionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+    var existId = '';
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Type of Transaction invalid.', 403)
+    }
+
+    const checkCategory = await categoriesRepository.findOne({
+      where: { title: category }
+    })
+
+    if (checkCategory) {
+      existId = checkCategory.id
+    } else {
+
+      const categoryCreate = categoriesRepository.create({
+        title: category,
+      })
+
+      const categorySave = await categoriesRepository.save(categoryCreate)
+      existId = categorySave.id;
+    }
+
+
+    const transactionCreate = transitionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: existId
+    })
+
+    const transaction = await transitionsRepository.save(transactionCreate)
+
+    return transaction;
   }
 }
 
